0ee255f25e175ed35762bd4449a3b2488e77505f
Tigri Valdivia, Antonio {DISB~SANT CUGAT DIA} <antonio.tigri_valdivia@roche.com>
2024-05-10 11:16:57 +0200

    More refactors and bugfixes, all Linked List implementations finished.

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -36,7 +36,7 @@ class LinkedList:
         self.tail: Node = None
         if elements:
             for e in elements:
-                self.append(e)
+                self.append(e if not isinstance(elements, LinkedList) else e.data)
 
     def __len__(self: Self) -> int:
         return self.size
@@ -201,9 +201,7 @@ class LinkedList:
             return node
         raise IndexError("linked list index out of range")
 
-    def insert(self: Self, data: object, position: int = None) -> None:
-        if position is None:
-            return self.append(data)
+    def insert(self: Self, data: object, position: int) -> None:
         position += self.size if position < 0 else 0
         if position == len(self):
             return self.append(data)
@@ -300,7 +298,7 @@ class LinkedList:
         else:
             node = self.get(start - 1)
             node.next = self.get(stop - 1).next
-            if node.next is None:
+            if stop == len(self):
                 self.tail = node
 
         self.size -= stop - start
@@ -378,8 +376,11 @@ class CircularLinkedList(LinkedList):
 
     def __delitem__(self: Self, key: int | slice) -> None:
         if isinstance(key, int):
-            return super().__delitem__(key % len(self))
-        return super().__delitem__(key)
+            super().__delitem__(key % len(self))
+        else:
+            super().__delitem__(key)
+        if self.head is not None:
+            self.tail.next = self.head
 
     def __iter__(self: Self) -> Self:
         self.iter_node = self.head
@@ -390,9 +391,9 @@ class CircularLinkedList(LinkedList):
         if self.i >= len(self):
             raise StopIteration
         self.i += 1
-        data = self.iter_node.data
+        node = self.iter_node
         self.iter_node = self.iter_node.next
-        return data
+        return node
 
     def __str__(self: Self) -> str:
         return f"CircularLinkedList{tuple([node.data for node in self])}"
@@ -413,81 +414,34 @@ class CircularLinkedList(LinkedList):
             raise IndexError(
                 "cirlcular linked list index out of range, insertion does not support cyclic indexing"
             )
-        self.size += 1
-        node = Node(data)
-        if position == 0:
-            if not self.head:
-                self.head = self.tail = node
-                self.head.next = self.tail.next = node
-            node.next = self.head
-            self.head = node
-            self.tail.next = self.head
-        else:
-            prev = self.get(position - 1)
-            node.next = prev.next
-            prev.next = node
+        super().insert(data, position)
+        self.tail.next = self.head
 
     def append(self: Self, data: object) -> None:
-        self.size += 1
-        node = Node(data, self.head)
-        if not self.tail:
-            self.head = self.tail = node
-            self.head.next = self.tail.next = node
-            return
-        self.tail.next = node
-        self.tail = node
+        super().append(data)
+        self.tail.next = self.head
 
     def join(self: Self, linked_list: LinkedList) -> CircularLinkedList:
-        if self.head is None:
-            self.head = linked_list.head
-        else:
-            self.tail.next = linked_list.head
-        linked_list.tail.next = self.head
-        self.tail = linked_list.tail
-        self.size += linked_list.size
+        super().join(linked_list)
+        if self.head is not None:
+            self.tail.next = self.head
         return self
 
     def couple(self: Self, linked_list: LinkedList, position: int) -> Self:
         position += len(self) if position < 0 else 0
-        if position == len(self):
-            return self.join(linked_list)
-        if position == 0:
-            linked_list.tail.next = self.head
-            self.head = linked_list.head
-            self.tail.next = self.head
-        elif position > len(self):
+        if position < 0 or position > len(self):
             raise IndexError(
                 "cirlcular linked list index out of range, insertion does not support cyclic indexing"
             )
-        else:
-            node = self.get(position - 1)
-            linked_list.tail.next = node.next
-            node.next = linked_list.head
-        self.size += linked_list.size
+        super().couple(linked_list, position)
+        if self.head is not None:
+            self.tail.next = self.head
         return self
 
     def pop(self: Self, position: int = None) -> Node:
-        if position is None:
-            if len(self) <= 0:
-                raise TypeError("linked list is empty")
-            position = len(self) - 1
-        position %= len(self)
-        self.size -= 1
-        if position == 0:
-            node = self.head
-            if len(self) == 0:
-                self.head = self.tail = None
-            else:
-                self.head = self.head.next
-                self.tail.next = self.head
-        else:
-            prev = self.get(position - 1)
-            node = prev.next
-            if position == len(self):
-                self.tail = prev
-                prev.next = self.head
-            else:
-                prev.next = node.next
+        node = super().pop(position % len(self) if position is not None else None)
+        if self.head is not None:
+            self.tail.next = self.head
         return node
 
     def decouple(self, start: int = 0, stop: int = None) -> Self:
@@ -499,24 +453,9 @@ class CircularLinkedList(LinkedList):
             raise IndexError(
                 "circular list index out of range, non-inclusion stop does not support cyclic indexing"
             )
-        if start > stop:
-            start, stop = stop, start
-        if stop - start == len(self):
-            self.head = None
-            self.tail = None
-            self.size = 0
-            return self
-        if start == 0:
-            self.head = self.get(stop)
-        else:
-            node = self.get(start - 1)
-            if stop == len(self):
-                self.tail = node
-            else:
-                node.next = self.get(stop - 1).next
-        self.tail.next = self.head
-
-        self.size -= stop - start
+        super().decouple(start, stop)
+        if self.head is not None:
+            self.tail.next = self.head
         return self
 
     def copy(self: Self) -> CircularLinkedList:
@@ -541,7 +480,127 @@ class DoublyLinkedList(LinkedList):
         self.tail: DNode = None
         if elements:
             for e in elements:
-                self.append(e)
+                self.append(e if not isinstance(elements, LinkedList) else e.data)
+
+    def __getitem__(self: Self, key: int | slice) -> Self | object:
+        return super().__getitem__(key, DoublyLinkedList)
+
+    def __setitem__(
+        self: Self, key: int | slice, value: object | Node | Iterable | LinkedList
+    ) -> None:
+        if isinstance(key, slice):
+            if not isinstance(value, Iterable):
+                raise TypeError(
+                    "invalid argument type, slice assignment only accepts iterables"
+                )
+            start = 0 if key.start is None else key.start
+            stop = len(self) if key.stop is None else key.stop
+            step = 1 if key.step is None else key.step
+            if step == 1:
+                del self[start:stop]
+                self.couple(
+                    value if isinstance(value, LinkedList) else DoublyLinkedList(value),
+                    start,
+                )
+            else:
+                slice_size = (stop - start) // abs(step) + 1 * (
+                    (stop - start) % abs(step) != 0
+                )
+                if slice_size != len(value):
+                    raise ValueError(
+                        f"attempt to assign sequence of size {len(value)} to extended slice of size {slice_size}"
+                    )
+                step_count = 0
+                value_count = 0
+                if step > 1:
+                    for i in range(len(self)):
+                        if start <= i < stop:
+                            if step_count % step == 0:
+                                self[i] = value[value_count]
+                                value_count += 1
+                            step_count += 1
+                        elif i >= stop:
+                            break
+                elif step < 0:
+                    node = self[stop - 1]
+                    for _ in range(start, stop):
+                        if step_count % abs(step) == 0:
+                            node.data = value[value_count]
+                            value_count += 1
+                        step_count += 1
+                        node = node.prev
+                        if node is None:
+                            break
+
+        elif isinstance(key, int):
+            self.get(key).data = value.data if isinstance(value, Node) else value
+
+        else:
+            raise TypeError("invalid argument type")
+
+    def __delitem__(self: Self, key: int | slice) -> None:
+        if isinstance(key, slice):
+            start = 0 if key.start is None else key.start
+            stop = len(self) if key.stop is None else key.stop
+            step = 1 if key.step is None else key.step
+            if step == 1:
+                self.decouple(start, stop)
+            elif step > 1:
+                step_count = 0
+                node = self[start]
+                for _ in range(start, stop):
+                    if step_count % step == 0:
+                        if node == self.head:
+                            self.head = node.next
+                            if self.head is not None:
+                                self.head.prev = None
+                            if node == self.tail:
+                                self.tail = None
+                        elif node == self.tail:
+                            self.tail = node.prev
+                            if self.tail is not None:
+                                self.tail.next = None
+                        else:
+                            node.prev.next = node.next
+                            node.next.prev = node.prev
+                            if node == self.tail:
+                                self.tail = node.prev
+                        self.size -= 1
+                    node = node.next
+                    step_count += 1
+                    if node is None:
+                        break
+            elif step < 0:
+                step_count = 0
+                node = self[stop - 1]
+                for i in range(start, stop):
+                    if step_count % abs(step) == 0:
+                        if node == self.tail:
+                            self.tail = node.prev
+                            if self.tail is not None:
+                                self.tail.next = None
+                            if node == self.head:
+                                self.head = None
+                        elif node == self.head:
+                            self.head = node.next
+                            if self.head is not None:
+                                self.head.prev = None
+                        else:
+                            node.prev.next = node.next
+                            node.next.prev = node.prev
+                            if node == self.head:
+                                self.head = node.next
+                        self.size -= 1
+                    node = node.prev
+                    step_count += 1
+                    if node is None:
+                        break
+
+        elif isinstance(key, int):
+            self.pop(key)
+
+        else:
+            raise TypeError("invalid argument type")
 
     def __str__(self: Self) -> str:
         return f"DoublyLinkedList{tuple([node.data for node in self])}"
@@ -596,6 +655,11 @@ class DoublyLinkedList(LinkedList):
         return self
 
     def couple(self: Self, linked_list: LinkedList, position: int) -> Self:
+        linked_list = (
+            DoublyLinkedList(linked_list)
+            if not isinstance(linked_list, DoublyLinkedList)
+            else linked_list
+        )
         if position == len(self):
             return self.join(linked_list)
         if position == 0:
@@ -611,5 +675,159 @@ class DoublyLinkedList(LinkedList):
         self.size += linked_list.size
         return self
 
+    def pop(self: Self, position: int = None) -> Node:
+        if position is None:
+            if len(self) <= 0:
+                raise TypeError("linked list is empty")
+            position = len(self) - 1
+        position += len(self) if position < 0 else 0
+        if position < 0 or position >= len(self):
+            raise IndexError("linked list index out of range")
+        self.size -= 1
+        if position == 0:
+            node = self.head
+            self.head = self.head.next
+            if len(self) == 0:
+                self.tail = None
+            else:
+                self.head.prev = None
+        else:
+            prev = self.get(position - 1)
+            node = prev.next
+            if position == len(self):
+                self.tail = prev
+                prev.next = None
+            else:
+                node.next.prev = prev
+                prev.next = node.next
+        return node
+
+    def decouple(self, start: int = 0, stop: int = None) -> Self:
+        if stop is None:
+            stop = len(self)
+        start += len(self) if start < 0 else 0
+        stop += len(self) if stop < 0 else 0
+        if start < 0 or stop < 0 or start >= len(self) or stop > len(self):
+            raise IndexError("index out of range")
+        if start > stop:
+            start, stop = stop, start
+        if stop - start == len(self):
+            self.head = None
+            self.tail = None
+            self.size = 0
+            return self
+        if start == 0:
+            self.head = self.get(stop)
+            self.head.prev = None
+            if self.head.next is None:
+                self.tail = self.head
+        else:
+            node = self.get(start - 1)
+            node.next = self.get(stop - 1).next
+            node.next.prev = node
+            if node.next is None:
+                self.tail = node
+
+        self.size -= stop - start
+        return self
+
     def copy(self: Self) -> DoublyLinkedList:
         return DoublyLinkedList((data for data in self))
+
+
+class CircularDoublyLinkedList(DoublyLinkedList, CircularLinkedList):
+    def __init__(self: Self, elements: Iterable = None) -> None:
+        DoublyLinkedList.__init__(self, elements)
+
+    def __getitem__(self: Self, key: int | slice) -> Self | object:
+        if isinstance(key, int):
+            return LinkedList.__getitem__(
+                self, key % len(self), CircularDoublyLinkedList
+            )
+        return LinkedList.__getitem__(self, key, CircularDoublyLinkedList)
+
+    def __setitem__(
+        self: Self, key: int | slice, value: object | Iterable | LinkedList
+    ) -> None:
+        if isinstance(key, int):
+            return DoublyLinkedList.__setitem__(self, key % len(self), value)
+        return DoublyLinkedList.__setitem__(self, key, value)
+
+    def __delitem__(self: Self, key: int | slice) -> None:
+        if isinstance(key, int):
+            DoublyLinkedList.__delitem__(self, key % len(self))
+        else:
+            DoublyLinkedList.__delitem__(self, key)
+        if self.head is not None:
+            self.head.prev, self.tail.next = self.tail, self.head
+
+    def __iter__(self: Self) -> Self:
+        return CircularLinkedList.__iter__(self)
+
+    def __next__(self: Self) -> DNode:
+        return CircularLinkedList.__next__(self)
+
+    def __str__(self: Self) -> str:
+        return f"CircularDoublyLinkedList{tuple([node.data for node in self])}"
+
+    def __mul__(self: Self, value: int) -> None:
+        return LinkedList.__mul__(self, value, CircularDoublyLinkedList)
+
+    def insert(self: Self, data: object, position: int = None) -> None:
+        if position is None:
+            return self.append(data)
+        position += self.size if position < 0 else 0
+        if position == len(self):
+            return self.append(data)
+        if position < 0 or position > len(self):
+            raise IndexError(
+                "cirlcular linked list index out of range, insertion does not support cyclic indexing"
+            )
+        DoublyLinkedList.insert(self, data, position)
+        self.head.prev, self.tail.next = self.tail, self.head
+
+    def append(self: Self, data: object) -> None:
+        DoublyLinkedList.append(self, data)
+        self.head.prev, self.tail.next = self.tail, self.head
+
+    def join(self: Self, linked_list: LinkedList) -> Self:
+        DoublyLinkedList.join(self, linked_list)
+        if self.head is not None:
+            self.head.prev, self.tail.next = self.tail, self.head
+        return self
+
+    def couple(self: Self, linked_list: LinkedList, position: int) -> Self:
+        position += len(self) if position < 0 else 0
+        if position < 0 or position > len(self):
+            raise IndexError(
+                "cirlcular linked list index out of range, insertion does not support cyclic indexing"
+            )
+        DoublyLinkedList.couple(self, linked_list, position)
+        if self.head is not None:
+            self.head.prev, self.tail.next = self.tail, self.head
+        return self
+
+    def pop(self: Self, position: int = None) -> Node:
+        node = DoublyLinkedList.pop(
+            self, position % len(self) if position is not None else None
+        )
+        if self.head is not None:
+            self.head.prev, self.tail.next = self.tail, self.head
+        return node
+
+    def decouple(self, start: int = 0, stop: int = None) -> Self:
+        if stop is None:
+            stop = len(self)
+        start += len(self) if start < 0 else 0
+        stop += len(self) if stop < 0 else 0
+        if start < 0 or stop < 0 or start >= len(self) or stop > len(self):
+            raise IndexError(
+                "circular list index out of range, non-inclusion stop does not support cyclic indexing"
+            )
+        DoublyLinkedList.decouple(self, start, stop)
+        if self.head is not None:
+            self.head.prev, self.tail.next = self.tail, self.head
+        return self
+
+    def copy(self: Self) -> CircularDoublyLinkedList:
+        return CircularDoublyLinkedList((data for data in self))
